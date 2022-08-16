76a53462cbf344b1c4942d68a20d2537796a2cff
Sunil Reddy <sunilreddy@Sunils-MacBook-Air.local>
2022-08-17 00:32:42 +0530

    change score limit

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //2. check the current player's score >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
       diceEl.classList.add('hidden');
